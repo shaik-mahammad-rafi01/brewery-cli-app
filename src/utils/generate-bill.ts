fd71bacc8189d7160dc7a5de3a7ac29a88c63ea7
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-06-17 16:39:33 +0530

    Add payment confirmation condition

--- src/utils/generate-bill.ts
+++ src/utils/generate-bill.ts
@@ -1,6 +1,8 @@
+import { startBilling } from "../app";
 import { SelectedItem } from "../interfaces/order-item";
 import { Product } from "../interfaces/product";
 import { selectedItems } from "./order-product";
+import { ask } from "./readline-module";
 
 export async function billing(): Promise<SelectedItem[]> {
    
@@ -19,5 +21,19 @@ export async function billing(): Promise<SelectedItem[]> {
         total -= discount;
         console.log("Discount is applied (10% off for if bill > 1000)")
     }
-    return selectedItems;
+    console.log(`\n Total Payable Amount : ${total}`)
+    const Paid = (await ask("Paid? (Yes/No) : ")).trim().toLowerCase();
+    let paid = ""
+    while (true) {
+        paid = (await ask("Paid? (Yes/No): ")).trim().toLowerCase();
+
+        if (paid === "yes") {
+            console.log("✅ Payment successful. Ready for next customer.\n");
+            return startBilling();
+        } else if (paid === "no") {
+            console.log("❌ Payment not done. Please pay to proceed.");
+        } else {
+            console.log("⚠️ Invalid response. Please enter 'yes' or 'no'.");
+        }
+    }
 }
