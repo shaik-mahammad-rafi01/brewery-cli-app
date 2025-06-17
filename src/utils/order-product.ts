dab8ea00c7e06fcda0b06977a85036f90acf3b19
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-06-17 15:52:27 +0530

    Add cup size checking condition

--- src/utils/order-product.ts
+++ src/utils/order-product.ts
@@ -13,12 +13,23 @@ export async function orderProduct(): Promise<SelectedItem[]> {
             console.log("❌ Product not found. Try again.");
             continue;
         }
-        
+
         const enteredQuantity = await ask("Enter the quantity: ");
         const quantity = parseInt(enteredQuantity);
         if (isNaN(quantity) || quantity <= 0) {
             console.log("❌ Please enter a valid quantity.");
             continue;
+        }
+
+        let cupsize: cupSize | undefined;
+        if (product.category === "Beverages") {
+            const size = (await ask("Enter cup size (small, medium, large): ")).trim().toLowerCase();
+            if (!["small", "medium", "large"].includes(size)) {
+                console.log("Invalid cup size. Defaulting to 'small'.");
+                cupsize = "small";
+            } else {
+                cupsize = size as cupSize;
+            }
         }
     }
     return selectedItems
