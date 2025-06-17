ffcecc51957b5b567d7ed5ea0ba595dde53f9806
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2025-06-17 12:26:57 +0530

    Add products data with types and interface

---
+++ src/data/product-data.ts
@@ -0,0 +1,11 @@
+import { Product } from "../interfaces/product";
+export const products: Product[] = [
+    { name: "Almond milk", price: 150, type: "vegan", category: "Beverages" },
+    { name: "Coffee", price: 180, type: "vegetarian", category: "Beverages" },
+    { name: "cake", price: 120, type: "vegetarian", category: "Desserts" },
+    { name: "checken wings", price: 250, type: "non-vegetarian", category: "Snacks" },
+    { name: "Lemone juice", price: 100, type: "vegan", category: "Beverages" },
+    { name: "laddu", price: 90, type: "vegetarian", category: "Sweets" },
+    { name: "Paneer curry", price: 200, type: "vegetarian", category: "Snacks" },
+
+];
